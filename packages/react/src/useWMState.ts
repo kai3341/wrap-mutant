f39276abb0e366f3272f0a5eff0c38a67af40a14
Anton Kovalevich <kai3341@gmail.com>
2024-01-26 02:13:37 +0100

    Also allow to configure wrapCached::count parameter

--- packages/react/src/useWMState.ts
+++ packages/react/src/useWMState.ts
@@ -6,23 +6,28 @@ export type WMStateOptions<A> = {
   bind?: boolean;
   deps?: any[];
   args?: A;
+  count?: number;
 };
 
 const WMStateFactory = <A, T>(
   factory: (args: A) => T,
   args: A,
   bind: boolean,
+  count?: number,
 ) => {
   const value = factory(args);
   if (bind) bindCallables(value);
-  return wrapCached(value);
+  return wrapCached(value, count);
 };
 
 export const useWMState = <A, T>(
   factory: () => T,
-  { deps = [], bind = false, args }: WMStateOptions<A> = {},
+  { deps = [], bind = false, args, count }: WMStateOptions<A> = {},
 ) => {
-  const wrappedValue = useMemo(() => WMStateFactory(factory, args, bind), deps);
+  const wrappedValue = useMemo(
+    () => WMStateFactory(factory, args, bind, count),
+    deps,
+  );
   const [state, setState] = useState(wrappedValue);
 
   const updateState = useCallback(
